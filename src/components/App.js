8c7af4058799092616b25071606ffb60c60d4d3e
Polash Biswas <polashbiswasit19@gmail.com>
2023-06-10 19:21:09 +0530

    submitting code-Sat Jun 10 2023 19:21:07 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      I am learning React. My life is getting better.
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
